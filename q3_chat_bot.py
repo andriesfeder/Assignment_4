f23f44f72cd63d5932f45b888bc61fe28e4fb613
andriesfeder <afeder@Andriess-MacBook-Pro.local>
2016-12-05 00:19:47 -0500

    Only Endings Remain
    Opens file, creates dictionary, takes input, checks last word of input.
     Need to add Endings (see bear) and should be done

--- q3_chat_bot.py
+++ q3_chat_bot.py
@@ -2,10 +2,90 @@
 
 import sys
 import string
+import random
 
 def main():
 
-    
+    dictionary = {}
+    eos = {}
+
+    for word in sys.argv[1:]:
+
+        word1 = ''
+        word2 = ''
+        spare = ''
+
+        myfile = open( word , "r")
+        data = myfile.read().replace('\n', ' ')
+        myfile.close();
+        data = data.lower()
+        data = data.replace('-', ' ')
+        data = data.replace('.', ' END ')
+        data = data.replace('?', ' END ')
+        data = data.replace('!', ' END ')
+        data = data.translate(None, string.punctuation)
+        data = data.replace(' END ', '.')
+
+        for letter in data:
+            if letter == ' ':
+                if len(word1) == 0:
+                    continue
+                else :
+                    break
+            else :
+                word1 = word1 + letter
+
+        for letter in data:
+            #if letter is a space and length of word1 or word2 is 0, continues
+            #if letter is a space and length of word1 or word2 != 0, checks if
+            #dictionary has the word as a key. word = word1+'-'+word2
+            if letter == ' ':
+                if len(word1) == 0:
+                    continue
+                elif len(word2) == 0:
+                    continue
+                if dictionary.has_key(word1) :
+                    #needs to be fixed
+                    dictionary[word1] = dictionary[word1] + (word2,)
+                    spare = word1
+                    word1 = word2
+                    word2 = ''
+                else :
+                    dictionary[word1] = (word2,);
+                    spare = word1
+                    word1 = word2
+                    word2 = ''
+            elif letter == '.' :
+                if eos.has_key(word1) :
+                    #needs to be fixed
+                    eos[word1] = eos[word1] + (spare,)
+                else :
+                    eos[word1] = (spare,);
+            else :
+                word2 = word2+letter
+    count = 0
+    while count == 0:
+        output = ''
+        spare = raw_input("Send: ");
+        spare = spare.translate(None, string.punctuation)
+        spare = spare[::-1]
+        word1 = ''
+        for letter in spare:
+            if letter == ' ':
+                if len(word1) == 0:
+                    continue
+                else :
+                    break
+            else :
+                word1 = letter + word1
+        if dictionary.has_key(word1) :
+            word2 = random.choice(dictionary[word1])
+            word2 = word2.capitalize()
+            output = word2
+        else :
+            word2 = random.choice(dictionary.keys())
+            word2 = word2.capitalize()
+            output = word2
 
 if __name__ == '__main__':
     main()
